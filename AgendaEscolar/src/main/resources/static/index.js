f7310837d501591eed075387d7073118f8d00ddb
emanuelicalza <emanuelimanu06@gmail.com>
2024-10-29 20:55:35 -0300

    mudei o codigo js para outro arquivo

--- AgendaEscolar/src/main/resources/static/index.js
+++ AgendaEscolar/src/main/resources/static/index.js
@@ -72,34 +72,3 @@ document.addEventListener('DOMContentLoaded', function() {
     calendar.render();
 });
 
-
-function salvarCadastrar(element){
-    let nome = $("#nome").val();
-    let email = $("#emailCadastro").val();
-    let senha = $("#senha").val();
-    let confirmarSenha = $("#confirmarSenha").val();
-    let dataNascimento = $("#dataNascimento").val();
-
-    $.ajax({
-            url: "/cadastro",
-            method: "post",
-            data: {
-                nome: nome,
-                email: email,
-                senha: senha,
-                confirmarSenha: confirmarSenha,
-                dataNascimento: dataNascimento
-            },
-            success: function(data) {
-                $("#activity-container").append(data);
-                $("#nome").val(''); // Limpa o campo de atividade
-                $("#email").val(''); // Limpa o campo de data
-                $("#senha").val('');
-                $("#confirmarSenha").val('');
-                $("#dataNascimento").val('');
-            },
-            error: function() {
-                alert("deu ruim");
-            }
-        });
-}
