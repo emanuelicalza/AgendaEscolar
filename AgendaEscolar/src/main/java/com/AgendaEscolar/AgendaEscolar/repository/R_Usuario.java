56d24e9d9b7ab67feec307da350f5de46fa13b22
emanuelicalza <emanuelimanu06@gmail.com>
2024-10-30 19:49:56 -0300

    fiz o login (e resolvi os bo de conflito)

--- AgendaEscolar/src/main/java/com/AgendaEscolar/AgendaEscolar/repository/R_Usuario.java
+++ AgendaEscolar/src/main/java/com/AgendaEscolar/AgendaEscolar/repository/R_Usuario.java
@@ -4,6 +4,9 @@ import com.AgendaEscolar.AgendaEscolar.model.M_Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface R_Usuario extends JpaRepository<M_Usuarios, Long> {
+    Optional<M_Usuarios> findByEmail(String email); // Adiciona busca por email
 }
